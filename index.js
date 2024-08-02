07dd51c88eb82d99cb660ab15fb44a1dbc017947
Mansoor Musthafa <msmansoor523@gmail.com>
2024-08-02 10:59:31 +0530

    js 1st push

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+let a= .95;
+
+let b= 7.99;
+
+let c= b * 2;
+console.log(c + a)
+
+
+
+//   how to calculate percentage
+
+//   1% = 1/100
+
+// 10%= 10/100
+
+let d= 3/100;
+console.log(d);
+
+
