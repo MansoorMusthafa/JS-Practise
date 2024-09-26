6a5489fc9e068987ab947b0a2f0bd0f5336e5137
Mansoor Musthafa <msmansoor523@gmail.com>
2024-09-26 16:56:22 +0530

    new js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 // let c= b * 2;
 // console.log(c + a)
 
-
 // //   how to calculate percentage
 
 // //   1% = 1/100
@@ -15,13 +14,81 @@
 // let d= 3/100;
 // console.log(d);
 
-
 // console.log('Items'+" ("+(1 + 1)+"):"+" $"+(20.95 + 7.99))
 
-                                       // Variable
+// Variable
+
+// console.log(typeof 1)
+
+// let x = 0;
+// while ((x++ < 5, x < 3)) {
+//   console.log(x);+
+// }
+
+// function foo(a,b,c){
+//   arguments[0] = 10;
+//   console.log(c, arguments[0]);
+
+// }
+// foo(1,2,3)
+
+// for (let x= 1; x < 10; x++ ) {
+//   console.log(x);
+// }
+
+// let x = 1;
+// {
+//   let x = 2;
+//   console.log(x);
+// }
+// console.log(x);
+
+// let car = "red";
+
+// if ((car == "blue")) {
+//   console.log("1 if_else");
+// } else if ((car == "yellow")) {
+//   console.log("2 if_else");
+// } else if ((car == "red")) {
+//   console.log("3 if else");
+// } else if ((car == "pink")) {
+//   console.log("4 if_else");
+// } else {
+//   console.log("none");
+// }
+
+
+// let s= [1, 2, 3, 4]
+// console.log(s)
+
+
+// console.log("1 " + "- " + (1*1))
+
+
+// console.log("1","2","3","4\n5");
+
+// let x= "64";
+// let y = 8;
+//  x= Number(y)// To convert string to number
+//  console.log(typeof x)
+// console.log( x + y)
 
 
 
 
+//////////////////////////////////Adding  with string ///////////////////
+// let x= "64";
+// let y = 8;
 
-                                       
+// console.log( x + y)
+
+////////////////////////////////MUltiply  with string ///////////////////
+// let x= "64";
+// let y = 8;
+
+// console.log( x * y)
+
+   
+// let x = Boolean("False");  Js will not inside string letters , it will see only type , no matter what inside text ex("false") if we use Boolean()function it will convert to true 
+
+// console.log(x)
